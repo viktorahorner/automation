146995d6bcd3f4de563b883394994a6b99a4068c
viktorahorner <48084834+viktorahorner@users.noreply.github.com>
2026-02-07 10:10:39 +0100

    Update releasequarantine.ps1

--- releasequarantine.ps1
+++ releasequarantine.ps1
@@ -1,2 +1,2 @@
-Get-QuarantineMessage -QuarantineTypes HighConfPhis  -Direction Inbound -Domain chainiq.com -SenderAddress *@chainiq.com | Release-QuarantineMessage -ReleaseToAll -Verbose -WhatIf
-Get-QuarantineMessage -QuarantineTypes Phish  -Direction Inbound -Domain chainiq.com -SenderAddress *@chainiq.com | Release-QuarantineMessage -ReleaseToAll -Verbose -WhatIf
+Get-QuarantineMessage -QuarantineTypes HighConfPhis  -Direction Inbound -ReleaseStatus NOTRELEASED -Domain chainiq.com -SenderAddress *@chainiq.com | Release-QuarantineMessage -ReleaseToAll -Verbose -WhatIf
+Get-QuarantineMessage -QuarantineTypes Phish  -Direction Inbound -ReleaseStatus NOTRELEASED -Domain chainiq.com -SenderAddress *@chainiq.com | Release-QuarantineMessage -ReleaseToAll -Verbose -WhatIf
